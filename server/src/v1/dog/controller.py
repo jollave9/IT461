70348211c4133f8bab2e399493cebaf9f1c3f122
Jonathan Jubeth Ollave <jollave9@gmail.com>
2022-05-13 13:54:27 +0800

    exercise 3

--- server/src/v1/dog/controller.py
+++ server/src/v1/dog/controller.py
@@ -2,6 +2,7 @@ from flask import request, make_response, jsonify
 from v1.basecontroller import BaseController
 from v1.dog.model import DogModel
 
+
 class DogController(BaseController):
     _instance = None
 
@@ -12,14 +13,19 @@ class DogController(BaseController):
         print(request.json)
         resp = self._instance.create(request.json)
         if resp == False:
-            return make_response(jsonify({
-                "error": "Failed to add. There are items in your request that are invalid."
-            }), 400)
+            return make_response(
+                jsonify(
+                    {
+                        "error": "Failed to add. There are items in your request that are invalid."
+                    }
+                ),
+                400,
+            )
         return jsonify(resp)
 
     def check(self, dog_id, filters=None):
         if filters is not None:
-            filters['id'] = dog_id
+            filters["id"] = dog_id
         else:
             filters = {"id": dog_id}
         dog = self._instance.read(filters)
@@ -29,24 +35,30 @@ class DogController(BaseController):
 
     def get(self, dog_id=None):
         filters = {}
-        if 'fields' in request.args:
-            filters['fields'] = request.args['fields'].split(',')
+        if "fields" in request.args:
+            filters["fields"] = request.args["fields"].split(",")
         if dog_id is not None:
             dog = self.check(dog_id, filters)
             if not isinstance(dog, dict):
                 return dog
             return jsonify(dog)
-        filters['offset'] = int(request.args['offset']) if 'offset' in request.args else 0
-        filters['limit'] = int(request.args['limit']) if 'limit' in request.args else 5
+        filters["offset"] = (
+            int(request.args["offset"]) if "offset" in request.args else 0
+        )
+        filters["limit"] = int(request.args["limit"]) if "limit" in request.args else 5
         dogs = self._instance.read(filters)
         total = self._instance.read(filters, True)
-        return jsonify({
-            'metadata': {
-                'total': total,
-                'links': self.build_links(total, filters['offset'], filters['limit'])
-            },
-            'data': dogs
-        })
+        return jsonify(
+            {
+                "metadata": {
+                    "total": total,
+                    "links": self.build_links(
+                        total, filters["offset"], filters["limit"]
+                    ),
+                },
+                "data": dogs,
+            }
+        )
 
     def put(self, dog_id=None):
         if dog_id is not None:
@@ -54,12 +66,17 @@ class DogController(BaseController):
             if not isinstance(dog, dict):
                 return dog
             dog_data = request.json
-            dog_data['id'] = dog_id
+            dog_data["id"] = dog_id
             resp = self._instance.update(dog_data)
             if resp == False:
-                return make_response(jsonify({
-                    "error": "Failed to update. There are items in your request that are invalid."
-                }), 400)
+                return make_response(
+                    jsonify(
+                        {
+                            "error": "Failed to update. There are items in your request that are invalid."
+                        }
+                    ),
+                    400,
+                )
             return jsonify(resp)
         return jsonify(self._instance.update(request.json))
 
